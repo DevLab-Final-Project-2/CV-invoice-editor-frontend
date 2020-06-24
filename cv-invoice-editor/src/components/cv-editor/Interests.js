83ec457bbc2ec1fefcec40a6352b13343569075b
stashat2 <stasa.tadic1@gmail.com>
2020-06-24 12:39:23 +0200

    updated cv navigation

--- cv-invoice-editor/src/components/cv-editor/Interests.js
+++ cv-invoice-editor/src/components/cv-editor/Interests.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Input } from 'antd';
 const { TextArea } = Input;
 
-class WorkXp extends React.Component {
+class Interests extends React.Component {
     state = {}
     render() {
         return (
             <React.Fragment>
-                <h1>Work experience:</h1>
-                <TextArea rows={4} placeholder={'Enter your work experience'} />
+                <h1>Interests:</h1>
+                <TextArea rows={4} placeholder={'Enter your interests'} />
             </React.Fragment>);
     }
 }
 
-export default WorkXp;
+export default Interests;
--- cv-invoice-editor/src/components/cv-editor/cv.js
+++ cv-invoice-editor/src/components/cv-editor/cv.js
@@ -3,16 +3,72 @@ import { Button } from 'antd';
 import { Row, Col } from 'antd';
 import PersonalInfo from './personalInfo'
 import WorkXp from './WorkXp'
+import Qualif from './Qualif'
+import Education from './Education'
+import Interests from './Interests'
+import { UploadOutlined } from '@ant-design/icons'
+
 
 
 class CV extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            showHidePersonalInfo: true,
+            showHideWorkXp: false,
+            showHideQualif: false,
+            showHideEducation: false,
+            showHideInterests: false,
+        };
+        this.hideComponent = this.hideComponent.bind(this);
+    }
+    hideComponent(name) {
+        switch (name) {
+            case "showHidePersonalInfo":
+                this.setState({ showHidePersonalInfo: !this.state.showHidePersonalInfo });
+                this.setState({ showHideWorkXp: false });
+                this.setState({ showHideQualif: false });
+                this.setState({ showHideEducation: false });
+                break;
+            case "showHideWorkXp":
+                this.setState({ showHideWorkXp: !this.state.showHideWorkXp });
+                this.setState({ showHidePersonalInfo: false });
+                this.setState({ showHideQualif: false });
+                this.setState({ showHideEducation: false });
+                this.setState({ showHideInterests: false });
+
+                break;
+            case "showHideQualif":
+                this.setState({ showHideQualif: !this.state.showHideQualif });
+                this.setState({ showHidePersonalInfo: false });
+                this.setState({ showHideWorkXp: false });
+                this.setState({ showHideEducation: false });
+                this.setState({ showHideInterests: false });
+
+                break;
+            case "showHideEducation":
+                this.setState({ showHideEducation: !this.state.showHideEducation });
+                this.setState({ showHideWorkXp: false });
+                this.setState({ showHideQualif: false });
+                this.setState({ showHidePersonalInfo: false });
+                this.setState({ showHideInterests: false });
+                break;
+            case "showHideInterests":
+                this.setState({ showHideInterests: !this.state.showHideInterests });
+                this.setState({ showHideWorkXp: false });
+                this.setState({ showHideQualif: false });
+                this.setState({ showHidePersonalInfo: false });
+                this.setState({ showHideEducation: false });
+                break;
+            default:
+                this.setState({ showHidePersonalInfo: true });
+
+        }
     }
 
 
     render() {
+        const { showHidePersonalInfo, showHideWorkXp, showHideQualif, showHideEducation, showHideInterests } = this.state;
         return (
             <React.Fragment>
                 <div className='gridColor'>
@@ -21,24 +77,37 @@ class CV extends React.Component {
                         <hr className='hLine' />
                         <Row gutter={[8, 8]}>
                             <Col span={8} style={{ display: 'flex', flexDirection: 'column' }}>
-                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
+                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }}
+                                    onClick={() => this.hideComponent("showHidePersonalInfo")}>
                                     Personal Info
                                     </Button>
 
-                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
+                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }}
+                                    onClick={() => this.hideComponent("showHideWorkXp")}>
                                     Work experience
                                     </Button>
-                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
+                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }}
+                                    onClick={() => this.hideComponent("showHideQualif")}>
                                     Qualifications
                             </Button>
-                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
+                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }}
+                                    onClick={() => this.hideComponent("showHideEducation")}>
                                     Education
                             </Button>
-                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
+                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }}
+                                    onClick={() => this.hideComponent("showHideInterests")}>
                                     Interests
+                            </Button>
+                                <Button style={{ width: '70%', backgroundColor: 'white', color: 'rgba(17, 70, 73, 1)', borderRadius: '20px' }} icon={<UploadOutlined />}>
+                                    Submit CV
                             </Button>
                             </Col>
                             <Col span={16} className='column2'>
+
+                                {(showHidePersonalInfo && <PersonalInfo />) || (showHideWorkXp && <WorkXp />) || (showHideQualif && <Qualif />)
+                                    || (showHideEducation && <Education />) || (showHideInterests && <Interests />)}
+
+
                             </Col>
                         </Row>
                     </div>
