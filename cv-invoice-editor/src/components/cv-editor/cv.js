45866cab84014e2d092e164f327a92e58e800aec
stashat2 <stasa.tadic1@gmail.com>
2020-06-24 12:02:24 +0200

    404 page

--- cv-invoice-editor/src/components/cv-editor/cv.js
+++ cv-invoice-editor/src/components/cv-editor/cv.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Button } from 'antd';
 import { Row, Col } from 'antd';
 import PersonalInfo from './personalInfo'
 import WorkXp from './WorkXp'
 
+
 class CV extends React.Component {
-    state = {}
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+
     render() {
         return (
             <React.Fragment>
@@ -14,33 +19,28 @@ class CV extends React.Component {
                     <div className='cv'>
                         <h1>CV</h1>
                         <hr className='hLine' />
-                        <Router>
-                            <Row gutter={[8, 8]}>
-                                <Col span={8} style={{ display: 'flex', flexDirection: 'column' }}>
-                                    <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
-                                        <Link to='/personal-info'> Personal Info</Link>
+                        <Row gutter={[8, 8]}>
+                            <Col span={8} style={{ display: 'flex', flexDirection: 'column' }}>
+                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
+                                    Personal Info
                                     </Button>
-                                    <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
-                                        <Link to='/work-experience'>Work experience</Link>
+
+                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
+                                    Work experience
                                     </Button>
-                                    <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
-                                        Qualifications
+                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
+                                    Qualifications
                             </Button>
-                                    <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
-                                        Education
+                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
+                                    Education
                             </Button>
-                                    <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
-                                        Interests
+                                <Button style={{ width: '70%', backgroundColor: 'rgba(17, 70, 73, 1)', color: 'white', marginBottom: '10px' }} >
+                                    Interests
                             </Button>
-                                </Col>
-                                <Col span={16} className='column2'>
-                                    <Switch>
-                                        <Route path='/personal-info' component={PersonalInfo}></Route>
-                                        <Route path='/work-experience' component={WorkXp}></Route>
-                                    </Switch>
-                                </Col>
-                            </Row>
-                        </Router>
+                            </Col>
+                            <Col span={16} className='column2'>
+                            </Col>
+                        </Row>
                     </div>
                 </div>
             </React.Fragment>);
