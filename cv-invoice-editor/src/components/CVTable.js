d8958d138736d52f2c2cc853fe4737e4583cf1cb
Nuflix <djordje.vujicic@yandex.com>
2020-06-26 17:40:43 +0200

    dashboard

--- cv-invoice-editor/src/components/CVTable.js
+++ cv-invoice-editor/src/components/CVTable.js
@@ -1,13 +1,80 @@
-import React from 'react';
+/* import React from 'react'; */
 import { Table } from 'antd';
 import { Input } from 'antd';
+import React, { Component } from 'react';
+/* import { View, Text } from 'react-native'; */
 
 const { Search } = Input;
 
+
+
+
+
+
 class CVTable extends React.Component {
-    state = {}
+
+    constructor(props) {
+        super(props);
+        this.state = {dataSource : [
+            {
+                key: '1',
+                name: 'cv1',
+                PDF: 'cv1.pdf',
+            },
+            {
+                key: '2',
+                name: 'cv2',
+                PDF: 'cv.pdf'
+            },
+        ]};
+      }
+
+  componentWillMount() {
+      this.renderMyData();
+  }
+
+    renderMyData=()=>{
+        let token=localStorage.getItem("jwt").toString();
+            let cvid="5eef646733180507a0e4ffc7";
+            var myHeaders = new Headers();
+            myHeaders.append("Authorization", `Bearer ${token}`);
+            myHeaders.append("Content-Type", "application/json");
+            
+            var requestOptions = {
+              method: 'GET',
+              headers: myHeaders,
+              redirect: 'follow'
+            };
+            
+            fetch("http://78.155.34.239:3000/cvs", requestOptions)
+              .then(response => response.json())
+              .then((result)=>{ this.setState(
+                 {dataSource: result}
+              ) /* console.log(result); cvid=result.cv_id; */ })
+              .catch(function(error){ console.log('error', error);  });
+    }
+
+/*     fetching(){
+        let token=localStorage.getItem("jwt").toString();
+        let cvid="5eef646733180507a0e4ffc7";
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${token}`);
+        myHeaders.append("Content-Type", "application/json");
+        
+        var requestOptions = {
+          method: 'GET',
+          headers: myHeaders,
+          redirect: 'follow'
+        };
+        
+        fetch("http://78.155.34.239:3000/cvs", requestOptions)
+          .then(response => response.text())
+          .then(function(result){ console.log(result); cvid=result.cv_id; })
+          .catch(error => console.log('error', error));
+    } */
 
     render() {
+
         const dataSource = [
             {
                 key: '1',
@@ -42,7 +109,7 @@ class CVTable extends React.Component {
                     onSearch={value => console.log(value)}
                     style={{ width: 200, marginBottom: '1vh' }}
                 />
-                <Table dataSource={dataSource} columns={columns} />
+                <Table dataSource={this.state.dataSource} columns={columns} />
             </React.Fragment>
         );
     }
